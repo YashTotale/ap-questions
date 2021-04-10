45d9d9deba7fef816968eee0de433936eda7c298
Yash Totale <totaleyash@gmail.com>
2021-04-10 14:17:02 -0700

    Added Filter Component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -16,25 +16,38 @@ import { useAppDispatch } from "../Store";
 import { useFirestoreConnect } from "react-redux-firebase";
 
 //Material UI Imports
-import { CircularProgress, makeStyles, Theme } from "@material-ui/core";
+import {
+  CircularProgress,
+  makeStyles,
+  Paper,
+  Theme,
+  Typography,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => ({
+  loadingSpinner: {
+    marginTop: theme.spacing(2),
+  },
   home: {
     display: "flex",
     justifyContent: "center",
     width: "80%",
     margin: "auto",
-  },
-  loadingSpinner: {
-    marginTop: theme.spacing(2),
+    padding: theme.spacing(3),
   },
   filters: {
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
+    alignItems: "center",
     width: "100%",
+    padding: theme.spacing(2),
+  },
+  filtersHeading: {
+    margin: theme.spacing(1),
   },
   courseSelect: {
-    width: "100%",
+    width: "70%",
   },
 }));
 
@@ -80,26 +93,64 @@ const Filters: FC<FiltersProps> = ({
   selectedCourse,
 }) => {
   return (
-    <div className={classes.filters}>
-      <ThemedSelect
-        className={classes.courseSelect}
-        options={courses.map((course) => ({
-          label: course.title,
-          value: course.title,
-        }))}
-        value={
-          selectedCourse
-            ? {
-                label: selectedCourse,
-                value: selectedCourse,
-              }
-            : null
+    <Paper className={classes.filters} elevation={10}>
+      <Typography variant="h5" className={classes.filtersHeading}>
+        Filters
+      </Typography>
+      <Filter
+        name="Course"
+        action={
+          <ThemedSelect
+            className={classes.courseSelect}
+            options={courses.map((course) => ({
+              label: course.title,
+              value: course.title,
+            }))}
+            value={
+              selectedCourse
+                ? {
+                    label: selectedCourse,
+                    value: selectedCourse,
+                  }
+                : null
+            }
+            onChange={(option) =>
+              dispatch(changeCourse(option ? option.value : null))
+            }
+            placeholder="Select a course..."
+          />
         }
-        onChange={(option) =>
-          dispatch(changeCourse(option ? option.value : null))
-        }
-        placeholder="Select a course..."
       />
+    </Paper>
+  );
+};
+
+const useFilterStyles = makeStyles((theme) => ({
+  wrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  name: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+interface FilterProps {
+  name: string;
+  action: JSX.Element;
+}
+
+const Filter: FC<FilterProps> = ({ name, action }) => {
+  const classes = useFilterStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <Typography className={classes.name}>
+        <strong>{name}</strong>:
+      </Typography>
+      {action}
     </div>
   );
 };
