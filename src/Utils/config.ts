d541af2b633c00a140e4229df3abb6cf0008ba09
Yash Totale <totaleyash@gmail.com>
2021-04-10 09:41:57 -0700

    Set Up Firebase Config

--- src/Utils/config.ts
+++ src/Utils/config.ts
@@ -1,1 +1,9 @@
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: "AIzaSyCuYopIAa1DBD3Osw5dhph1GtonHVISqIs",
+  authDomain: "ap-questions.firebaseapp.com",
+  projectId: "ap-questions",
+  storageBucket: "ap-questions.appspot.com",
+  messagingSenderId: "729339902249",
+  appId: "1:729339902249:web:5dd7dc5a67c8472c659f38",
+  measurementId: "G-SR5CXEB8QP",
+};
